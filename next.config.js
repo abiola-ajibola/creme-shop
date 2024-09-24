86bc6e870cf7a83cb797b9afe66bf2370161c5b5
Abiola <ajibolaabiolaahmed@gmail.com>
2024-09-24 15:21:19 +0100

    feat: Add phone input

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['mui-tel-input'],
-}
+  transpilePackages: ["mui-tel-input"],
+  async redirects() {
+    return [{ source: "/", destination: "/products", permanent: true }];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
